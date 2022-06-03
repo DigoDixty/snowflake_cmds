9d19b2cef8058437743bcd1e557d77fee7835dfb
Rodrigo Cardoso <digo.bigdata@outlook.com>
2022-06-03 16:01:36 -0300

    Create get_format_table_parquet.sql

---
+++ get_format_table_parquet.sql
@@ -0,0 +1,33 @@
+create or replace procedure get_parquet_table(schemaname string , val_stage string, table_name string, file_type string)
+returns string
+language sql
+as $$
+DECLARE
+cmd string := '';
+query string := '';
+res resultset;
+
+val_stage2 := '@' ||val_stage || '/';
+
+BEGIN
+query :=  'SELECT COLUMN_NAME || '' '' || TYPE || '','' AS COLUMNS 
+           FROM TABLE (infer_schema(location=>''' || val_stage2 || table_name || '/LOAD00000001.parquet'', 
+           file_format=>'''|| file_type ||'''));';
+
+res := (EXECUTE IMMEDIATE query);
+
+let cur cursor for res;
+
+OPEN cur;
+    FOR row_variable IN cur DO
+        CMD := CMD || row_variable.COLUMNS;
+    END for;
+
+CMD := SUBSTRING(CMD,0,LENGTH(CMD)-1);
+
+CMD := 'CREATE TABLE IF NOT EXISTS '||schemaname||'.RAW_' || table_name || ' ('||CMD||');';
+
+return CMD;
+
+END
+$$;
