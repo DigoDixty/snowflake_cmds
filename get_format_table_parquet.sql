178833ee3766bec9a7f6415562f50be51ba5261f
Rodrigo Cardoso <digo.bigdata@outlook.com>
2022-06-24 12:35:30 -0300

    Update get_format_table_parquet.sql

--- get_format_table_parquet.sql
+++ get_format_table_parquet.sql
@@ -1,4 +1,4 @@
-create or replace procedure public.get_parquet_table(schemaname string , val_stage string, table_name string, file_type string)
+create or replace procedure public.get_parquet_table(schemaname string , val_stage string, table_name string, file_name string, file_type string)
 returns string
 language sql
 as $$
@@ -7,11 +7,9 @@ cmd string := '';
 query string := '';
 res resultset;
 
-val_stage2 := '@PUBLIC.' ||val_stage || '/';
-
 BEGIN
 query :=  'SELECT COLUMN_NAME || '' '' || TYPE || '','' AS COLUMNS 
-           FROM TABLE (infer_schema(location=>''' || val_stage2 || table_name || '/LOAD00000001.parquet'', 
+           FROM TABLE (infer_schema(location=>''@PUBLIC.' ||val_stage || '/' || file_name || ''', 
            file_format=>'''|| file_type ||'''));';
 
 res := (EXECUTE IMMEDIATE query);
@@ -20,15 +18,19 @@ let cur cursor for res;
 
 OPEN cur;
     FOR row_variable IN cur DO
-        CMD := CMD || row_variable.COLUMNS;
+        CMD := CMD || CHAR(10) || row_variable.COLUMNS;
     END for;
 
 CMD := SUBSTRING(CMD,0,LENGTH(CMD)-1);
 
 CMD := 'CREATE OR REPLACE TABLE '||schemaname||'.RAW_' || table_name || ' ('||CMD||');';
 
-EXECUTE IMMEDIATE CMD;
+--EXECUTE IMMEDIATE CMD;
 RETURN CMD;
 
 END
 $$;
+
+/*
+call public.get_parquet_table('public' , 'st_teste', 'MARD', 'MARD.parquet', 'file_format_parquet');
+*/
