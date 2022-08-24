1c662648a23f1d378248f124265dc6e9950e08d4
Rodrigo Andre Cardoso <digo.bigdata@outlook.com>
2022-08-24 03:02:22 -0300

    Update create_tasks_parquet.sql
    New param to use ( file_name )

--- create_tasks_parquet.sql
+++ create_tasks_parquet.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE PROCEDURE public.crete_tasks_parquet(schemaname string , table_name string, stage string, v_warehouse string, file_format string)
+CREATE OR REPLACE PROCEDURE public.crete_tasks_parquet(schemaname string , table_name string, stage string, v_warehouse string, file_name string, file_format string)
 RETURNS string
 LANGUAGE SQL
 AS $$
@@ -8,7 +8,7 @@ BEGIN
 query :=  ' CREATE OR REPLACE '|| schemaname || '.' || table_name || '_TASK 
             WAREHOUSE='|| v_warehouse ||' SCHEDULE = ''USING CRON 0 3 * * * America/Sao_Paulo''
             USER_TASK_TIMEOUT_MS = 86400000
-            AS CALL load_parquet_table('''|| schemaname ||''', '''||stage||''', '''||table_name||''', '''||file_format||''');';
+            AS CALL load_parquet_table('''|| schemaname ||''', '''||stage||''', '''||table_name||''', '''||file_name||''', '''||file_format||''');';
 
 EXECUTE IMMEDIATE query;
 
