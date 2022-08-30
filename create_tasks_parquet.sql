8c3eee763611cdc535a37becaadc0eae7f6e01dc
Rodrigo Andre Cardoso <digo.bigdata@outlook.com>
2022-08-30 17:39:31 -0300

    Update create_tasks_parquet.sql

--- create_tasks_parquet.sql
+++ create_tasks_parquet.sql
@@ -1,22 +1,21 @@
-CREATE OR REPLACE PROCEDURE public.crete_tasks_parquet(schemaname string , table_name string, stage string, v_warehouse string, file_name string, file_format string)
+CREATE OR REPLACE PROCEDURE public.create_tasks_parquet(schemaname string , table_name string, stage string, v_warehouse string, file_name string, file_format string)
 RETURNS string
 LANGUAGE SQL
 AS $$
 DECLARE
 query string := '';
 BEGIN
-query :=  ' CREATE OR REPLACE '|| schemaname || '.' || table_name || '_TASK 
+query1 :=  ' CREATE OR REPLACE TASK '|| schemaname || '.' || table_name || '_TASK 
             WAREHOUSE='|| v_warehouse ||' SCHEDULE = ''USING CRON 0 3 * * * America/Sao_Paulo''
             USER_TASK_TIMEOUT_MS = 86400000
             AS CALL load_parquet_table('''|| schemaname ||''', '''||stage||''', '''||table_name||''', '''||file_name||''', '''||file_format||''');';
 
-EXECUTE IMMEDIATE query;
+-- EXECUTE IMMEDIATE query1;
 
-query := 'ALTER TASK '|| schemaname || '.' || table_name || '_TASK RESUME;';
+query2 := 'ALTER TASK '|| schemaname || '.' || table_name || '_TASK RESUME;';
 
-EXECUTE IMMEDIATE query;
-RETURN query;
+-- EXECUTE IMMEDIATE query2;
+RETURN query1;
 
 END
 $$;
-
